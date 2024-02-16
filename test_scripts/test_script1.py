76c0b19ca034a575bbe1cbe01de2aebbbaf8448f
siban <siban@DESKTOP-SISA0NM>
2024-02-16 21:29:42 +0530

    code updated

--- test_scripts/test_script1.py
+++ test_scripts/test_script1.py
@@ -14,3 +14,4 @@ class TestScript1(BaseSetup):
         print("This is test script 1")
         print(self.driver.title)
         Excel.get_data(self.xl_path,'login',2,2)
+        print("Hello World")
